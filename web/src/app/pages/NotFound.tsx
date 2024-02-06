3aa8902cb999ef6753b554bdab68d05c9ddb1791
Kenny-Dev <66855201+0xkenny@users.noreply.github.com>
2024-02-06 02:47:57 +0000

    style: 404 with something

--- web/src/app/pages/NotFound.tsx
+++ web/src/app/pages/NotFound.tsx
@@ -1,3 +1,10 @@
 export default function NotFound() {
-  return <div>404 Not Found</div>;
+  return (
+    <div className='bg-zinc-100 dark:bg-zinc-950 h-screen w-full flex items-center justify-center flex-col gap-8 '>
+      <h1 className='text-9xl p-12 after:border after:border-white after:animate-blink after:ml-4'>
+        404
+      </h1>
+      <h3 className='text-5xl'>PAGE NOT FOUND</h3>
+    </div>
+  )
 }
